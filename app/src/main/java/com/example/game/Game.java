7d71244b638c27e1d1dec5ee3600db0bd8076928
angel francis <50880391+Angelfrancis@users.noreply.github.com>
2021-03-17 20:30:03 +0530

    game

--- app/src/main/java/com/example/game/Game.java
+++ app/src/main/java/com/example/game/Game.java
@@ -3,10 +3,13 @@ package com.example.game;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import static java.lang.Math.round;
 
 public class Game extends AppCompatActivity {
 
@@ -25,45 +28,47 @@ public class Game extends AppCompatActivity {
         user=(ImageView)findViewById(R.id.user);
         target=(ImageView)findViewById(R.id.target);
         TextView i=(TextView)findViewById(R.id.i);
-        tx=target.getX();
-        ty=target.getY();
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        final int height = displayMetrics.heightPixels;
+        final int width = displayMetrics.widthPixels;
+        final int tx=(round(width/2+width/4)/100)*100;
+        final int ty=(round(height/2+height/4)/100)*100;
+        target.setTranslationX(tx);
+        target.setTranslationY(ty);
         left.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                x=user.getTranslationX();
-                if(x<=0)
+                if(user.getTranslationX()<=tx)
                 {
-                    user.setTranslationX(x-50);
+                    user.setTranslationX(user.getTranslationX()+100);
                 }
             }
         });
         right.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                x=user.getTranslationX();
-                if(x>=350)
+                if(user.getTranslationX()>=0)
                 {
-                    user.setTranslationX(x+50);
+                    user.setTranslationX(user.getTranslationX()-100);
                 }
             }
         });
         up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                y=user.getTranslationY();
-                if(y<=0)
+                if(user.getTranslationY()<=0)
                 {
-                    user.setTranslationY(y-50);
+                    user.setTranslationX(user.getTranslationY()-100);
                 }
             }
         });
         down.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                y=user.getTranslationY();
-                if(y>=600)
+                if(user.getTranslationY()<=ty)
                 {
-                    user.setTranslationY(y+50);
+                    user.setTranslationX(user.getTranslationY()+100);
                 }
             }
         });
