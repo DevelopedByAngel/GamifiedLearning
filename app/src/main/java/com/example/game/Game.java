6d888cf6739a01c95a25b57e726c11712456d4b1
angel francis <50880391+Angelfrancis@users.noreply.github.com>
2021-03-18 20:05:44 +0530

    Game direction

--- app/src/main/java/com/example/game/Game.java
+++ app/src/main/java/com/example/game/Game.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import static java.lang.Math.round;
 
@@ -15,8 +16,8 @@ public class Game extends AppCompatActivity {
 
     Button left,down,up,right;
     ImageView user,target;
-    float x,y;
     float tx,ty;
+    TextView i;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +28,7 @@ public class Game extends AppCompatActivity {
         right=(Button)findViewById(R.id.right);
         user=(ImageView)findViewById(R.id.user);
         target=(ImageView)findViewById(R.id.target);
-        TextView i=(TextView)findViewById(R.id.i);
+        i=(TextView)findViewById(R.id.i);
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         final int height = displayMetrics.heightPixels;
@@ -36,42 +37,53 @@ public class Game extends AppCompatActivity {
         final int ty=(round(height/2+height/4)/100)*100;
         target.setTranslationX(tx);
         target.setTranslationY(ty);
-        left.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(user.getTranslationX()<=tx)
-                {
-                    user.setTranslationX(user.getTranslationX()+100);
-                }
-            }
-        });
         right.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(user.getTranslationX()>=0)
+                if(user.getTranslationX()<tx)
+                {
+                    user.setTranslationX(user.getTranslationX()+100);
+                }
+                checkWin();
+            }
+        });
+
+        left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(user.getTranslationX()>0)
                 {
                     user.setTranslationX(user.getTranslationX()-100);
                 }
+                checkWin();
             }
-            
         });
         up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(user.getTranslationY()<=0)
+                if(user.getTranslationY()>0)
                 {
-                    user.setTranslationX(user.getTranslationY()-100);
+                    user.setTranslationY(user.getTranslationY()-100);
                 }
+                checkWin();
             }
         });
         down.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(user.getTranslationY()<=ty)
+                if(user.getTranslationY()<ty)
                 {
-                    user.setTranslationX(user.getTranslationY()+100);
+                    user.setTranslationY(user.getTranslationY()+100);
                 }
+                checkWin();
             }
         });
+    }
+    public void checkWin()
+    {
+        if(user.getTranslationX()==target.getTranslationX() && user.getTranslationY()==target.getTranslationY())
+        {
+            i.setText("win");
+        }
     }
 }
